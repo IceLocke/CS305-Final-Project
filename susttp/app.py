0321896bb82343b656137b5e789304bc16a147b9
Artanisax <1415197174@qq.com>
2023-12-29 16:30:11 +0800

    reseting

--- susttp/app.py
+++ susttp/app.py
@@ -9,6 +9,8 @@ import susttp.response as resp
 from susttp.authmanager import AuthManager
 from susttp.encryptmanager import EncryptManager
 
+STREAM_READER_BUFFER_LENGTH = 4096
+
 
 class App:
 
@@ -120,16 +122,31 @@ class App:
             request = (await reader.readuntil(b'\r\n\r\n')).decode('utf-8')
         except asyncio.IncompleteReadError as e:
             print(e)
-        self.logger.info(f'Get request:\n{request}')
+        if request and len(request) < 1024:
+            self.logger.info(f'Get request:\n{request}')
+        else:
+            self.logger.info('Get request: [too long]')
         if request:
             request = req.parse(request)
             if self.encrypt_manager.in_process(request):
                 response = self.encrypt_manager.handle_request(request)
             else:
+                # get route
                 path, method = request.path, request.method
-                request.request_param, request.path_param, handler, request.anchor = self.route_handler(path)
+                request.request_param, request.path_param, handler, request.anchor = self.route_handler(path)# read body
+                # read body
                 if "Content-Length" in request.headers.keys():
-                    request.body = await reader.read(request.headers["Content-Length"])
+                    total_length = int(request.headers["Content-Length"])
+                    if total_length:
+                        buffer_length = STREAM_READER_BUFFER_LENGTH
+                        if total_length <= buffer_length:
+                            request.body = await reader.read()
+                        else:
+                            request.body = b''
+                            while len(request.body) < total_length:
+                                request.body = request.body + await reader.read(
+                                    max(buffer_length, total_length - len(request.body))
+                                )
                 if handler is None:
                     self.logger.info(f'Cannot find resource {request.path}')
                     response = resp.not_find_response()
@@ -152,14 +169,18 @@ class App:
                         response = self.auth_manager.entry_func(request)
                     if 'encryption-session' in request.headers.keys():
                         self.encrypt_manager.encyrpt_response(response)
-        else:
+        else:  # if no request
             response = resp.Response(status=400, reason_phrase='Bad Request')
 
+        # response
         res = response.build()
         # print("app.py: response=")
         print(res)
         writer.write(res)
-        self.logger.info(f'Response {res}')
+        if res and len(res) < 1024:
+            self.logger.info(f'Response: {res}')
+        else:
+            self.logger.info('Response: [too long]')
         await writer.drain()
         writer.close()
 
